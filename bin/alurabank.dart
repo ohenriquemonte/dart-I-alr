9d5e1beabe1b1651d4ffc368eb3bcde0adc37873
Henrique Montenegro <henriquemontenegro@MacBook-Pro-de-Henrique.local>
2021-03-12 15:28:27 -0300

    02.01

--- bin/alurabank.dart
+++ bin/alurabank.dart
@@ -1,5 +1,19 @@
-import 'package:alurabank/alurabank.dart' as alurabank;
+void main() {
+  ContaCorrente conta = new ContaCorrente();
+  conta.titular = "Timóteo";
+  conta.agencia = 123;
+  conta.conta = 1234;
+  conta.saldo = 150.0;
 
-void main(List<String> arguments) {
-  print('Hello world: ${alurabank.calculate()}!');
+  print('Titular: ${conta.titular}');
+  print('Agencia: ${conta.agencia}');
+  print('Conta: ${conta.conta}');
+  print('Saldo: ${conta.saldo}');
+}
+
+class ContaCorrente {
+  String titular;
+  int agencia;
+  int conta;
+  double saldo;
 }
